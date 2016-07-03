1413a0c80e2e822804290241e600b56ee3ed5bb2
Chris Sinclair <chris@quarterstaff.co.uk>
2016-07-03 20:08:08 +0100

    Proper_factors function

--- factors.py
+++ factors.py
@@ -1,8 +1,16 @@
 import itertools
 
 def factors(factor_list):
+    ans = proper_factors(factor_list)
+    tmp = 1
+    for i in factor_list:
+        tmp *= i
+    ans.append(i)
+    return ans
+
+def proper_factors(factor_list):
     ans = set([1])
-    for i in range(len(factor_list) + 1):
+    for i in range(len(factor_list)):
         for j in itertools.combinations(factor_list, i):
             tmp = 1
             for k in j:
