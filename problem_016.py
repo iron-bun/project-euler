8238275cfd8b845ced026d77a2ad55d21368e328
Chris Sinclair <chris@quarterstaff.co.uk>
2016-07-03 13:06:13 +0100

    Solution to problem 16

---
+++ problem_016.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#2i^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+
+ans = sum([int(i) for i in str(2**1000)])
+print(ans)
