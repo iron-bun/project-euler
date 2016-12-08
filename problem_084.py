bed8826a9425083a4ac2a4a33841bfae4430242f
Chris Sinclair <chris@quarterstaff.co.uk>
2016-12-08 16:55:38 +0000

    Refactored design

--- problem_084.py
+++ problem_084.py
@@ -18,6 +18,60 @@ throws = [(i,j) for i in die for j in die]
 #Initialise Markov chain to zero
 states = [[0 for i in range(120)] for board in range(120)]
 
+def goto_jail(square, board, value):
+    offset = (square // 40) * 40
+    increment(offset + 10, board, value)
+
+def chance_card(square, board, value):
+    offset   = (square//40) * 40
+
+    increment(offset, board, value/16)
+    goto_jail(square, board, value/16)
+    increment(offset+11, board, value/16)
+    increment(offset+24, board, value/16)
+    increment(offset+39, board, value/16)
+    increment(offset+5,  board, value/16)
+    increment(square-3,  board, value/16)
+
+    if square in [7, 47, 87]:
+        increment(offset+15, board, value * 2 / 16)
+        increment(offset+12, board, value / 16)
+
+    if square in [22, 62, 102]:
+        increment(offset+25, board, value * 2 / 16)
+        increment(offset+28, board, value / 16)
+
+    if square in [36, 76, 116]:
+        increment(offset+5, board, value * 2 / 16)
+        increment(offset+12, board, value / 16)
+
+    board[square] += value * 6 / 16
+
+def community_chest_card(square, board, value=1):
+    offset = (square // 40) * 40
+    increment(offset, board, value/16)
+    goto_jail(square, board, value/16)
+    board[square] += value * 14 / 16
+
+cards = {
+    2:  community_chest_card,
+    7:  chance_card,
+    17: community_chest_card,
+    22: chance_card,
+    30: goto_jail,
+    33: community_chest_card,
+    36: chance_card
+}
+
+def increment(square, board, value):
+    action = cards.get(square%40, None)
+
+    if action == None:
+        board[square] += value
+    else:
+       action(square, board, value)
+ 
+
 #Add all of the possible destinations from the current start square
 for start, board in enumerate(states):
 
@@ -34,51 +88,7 @@ for start, board in enumerate(states):
           if destination >= 120:
               destination = 10
 
-          board[destination] += 1
-
-#skew the boards to account for chance, community chest and go to jail
-for board in states:
-    #go to jail
-    for square in [30, 70, 110]:
-        offset = (square // 40) * 40
-        board[offset+10] += board[square]
-        board[square] = 0
-
-    #chance cards
-    for square in [7, 47, 87, 22, 62, 102, 36, 76, 116]:
-        offset = (square // 40) * 40
-        board[offset]    += board[square] / 16     #advance to go
-        board[offset+10] += board[square] / 16     #go to jail
-        board[offset+11] += board[square] / 16     #advance to C1
-        board[offset+24] += board[square] / 16     #advance to E3
-        board[offset+39] += board[square] / 16     #advance to H3
-        board[offset+5]  += board[square] / 16     #advance to R1
-        board[square-3]  += board[square] / 16     #go back 3 spaces
-
-    for square in [7, 47, 87]:
-        offset = (square // 40) * 40
-        board[offset+15] += board[square] * 2 / 16 #advance to next R
-        board[offset+12] += board[square] / 16     #advance to next U
-
-    for square in [22, 62, 102]:
-        offset = (square // 40) * 40
-        board[offset+25] += board[square] * 2 / 16 #advance to next R
-        board[offset+28] += board[square] / 16     #advance to next U
-
-    for square in [36, 76, 116]:
-        offset = (square // 40) * 40
-        board[offset+5]  += board[square] * 2 / 16 #advance to next R
-        board[offset+12] += board[square] / 16     #advance to next U
-
-    for square in [7, 47, 87, 22, 62, 102, 36, 76, 116]:
-        board[square]    *= 6 / 16                 #stay on square
-
-    #community chest cards
-    for square in [2, 17, 33, 42, 57, 73, 82, 97, 113]:
-        offset = (square // 40) * 40
-        board[offset]    += board[square] / 16 #advance to Go
-        board[offset+10] += board[square] / 16 #go to jail
-        board[square]    *= 14 / 16            #stay on square
+          increment(destination, board, 1)
 
     #Normalise all the probabilities to sum to 1
     for square in range(120):
@@ -87,9 +97,9 @@ for board in states:
 start = states[0]
 
 for i in range(500):
-  start = np.dot(start, states)
+    start = np.dot(start, states)
 
 start = [start[i] + start[i+40] + start[i+80] for i in range(40)]
 for i, j in sorted(enumerate(start), key=lambda p: p[1]):
-  print(i, j)
+    print(i, j)
 
